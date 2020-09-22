ffbd86a1a3fa011846f0418feff018074635244f
quarx1981 <66761607+quarx1981@users.noreply.github.com>
2020-09-22 10:48:16 +0800

    Update diy-part1.sh
    add openappfileter

--- diy-part1.sh
+++ diy-part1.sh
@@ -9,6 +9,8 @@
 # ACRH17 KERNEL 5.4
 #=============================================================
 
+filter_url='https://github.com/destan19/OpenAppFilter.git'       # AppFilter 地址
+
 # 启用 fw876/helloworld
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
@@ -16,3 +18,10 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 # sed -i '$a src-git lienol https://github.com/xiaorouji/openwrt-package' feeds.conf.default
 
 # git clone https://github.com/kenzok8/small.git
+
+echo '添加OpenAppFilter过滤器'
+git clone $filter_url package/OpenAppFilter
+echo 'CONFIG_PACKAGE_luci-app-oaf=y' >> R619AC.config
+echo 'CONFIG_PACKAGE_kmod-oaf=y' >> R619AC.config
+echo 'CONFIG_PACKAGE_appfilter=y' >> R619AC.config
+echo 'CONFIG_PACKAGE_luci-i18n-oaf-zh-cn=y'  >> R619AC.config
